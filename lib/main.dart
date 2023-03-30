abda96ccd228585d8365ffebbe33975ff852000e
Gaurav Bhat <42300572+Grbmax@users.noreply.github.com>
2023-03-30 15:41:42 +0530

    Manifest Updates

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,52 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.teal,
-        body: Container(),
+        body: SafeArea(
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: <Widget>[
+              Container(
+                height: double.infinity,
+                width: 100.0,
+                color: Colors.red,
+                child: Center(child: Text("Container 1")),
+              ),
+              Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Container(
+                    height: 100.0,
+                    width: 100.0,
+                    color: Colors.yellow,
+                    child: Center(child: Text("Container 2")),
+                  ),
+                  Container(
+                    height: 100.0,
+                    width: 100.0,
+                    color: Colors.green,
+                    child: Center(child: Text("Container 2")),
+                  )
+                ],
+              ),
+              Container(
+                height: double.infinity,
+                width: 100.0,
+                color: Colors.blue,
+                child: Center(child: Text("Container 3")),
+              ),
+            ],
+          ),
+        ),
       ),
-    ),
-  );
+    );
+  }
 }
